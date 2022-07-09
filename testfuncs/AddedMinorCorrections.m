24302d59ef1a4a81f4be16bce5e1de1a68f31c2b
Rabbit <74029843+db-code-rabbit@users.noreply.github.com>
2022-07-09 18:57:56 +0200

    Add files via upload

---
+++ testfuncs/AddedMinorCorrections.m
@@ -0,0 +1,6 @@
+
+%%
+function outputbool = AddedMinorCorrections()
+    outputbool =true;
+end
+%%
---
+++ testfuncs/plotHeatTransfer.m
@@ -0,0 +1,48 @@
+%
+%%
+datasim =nominal;
+%
+%%
+cd('..\testfuncs\');
+if isfile('datasim')
+    delete datasim.mat;
+    save datasim datasim;
+else
+    save datasim datasim;
+end
+load datasim.mat;
+
+%
+%% 
+IFmodificationIs =AddedMinorCorrections();
+
+%
+%%
+figure, plot(datasim.t./60, datasim.XXX,'r', datasim.t./60, datasim.UUU,'b',datasim.t./60, datasim.OOO,'k',...
+    datasim.t./60, datasim.TTT,'g', datasim.t./60, datasim.SSS,'y', datasim.t./60, -datasim.RRR,'c'); 
+lg =legend('QL_{TOT}','Jtransfer.h','pdV','-QdotLS2', 'QdotWL2',...
+    'Jcd.h','Location','northeast'); title(lg,"Heat transfers in liquid phase"); 
+ylabel('Heat transfers in LIQUID - ET [Watts]',Fontweight='bold');
+xlabel('Time in minutes',Fontweight='bold');grid on; axis on;
+
+if (IFmodificationIs ==true)
+    ylim([-2000 8000]);
+else
+    ylim([-2000 3000]);
+end
+
+%
+%%
+figure, plot(datasim.t./60, datasim.WWW,'r', datasim.t./60, -datasim.OOO,'b',datasim.t./60, datasim.QQQ,'k',...
+    datasim.t./60, datasim.NNN,'g', datasim.t./60, datasim.MMM,'y', datasim.t./60, datasim.RRR,'c'); 
+lg =legend('QV_{TOT}','pdV','Jvalve.h','- QdotVS2', 'QdotWL2',...
+    'Jcd.h','Location','southeast'); title(lg,"Heat transfers in gas phase"); 
+ylabel('Heat transfers in VAPOR - ET [Watts]',Fontweight='bold');
+xlabel('Time in minutes',Fontweight='bold');grid on; axis on;
+
+if (IFmodificationIs ==true) 
+    ylim([-2500 1500]);
+else 
+    ylim([-1500 1500]);
+end
+%%
